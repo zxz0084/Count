191d4dccc5cfdc5fd0bdb8714dad545c53979813
zxz0084 <Vilaan@163.com>
2021-09-28 21:15:38 +0800

    eighth

--- app/src/main/java/com/swufestu/count/FirActivity.java
+++ app/src/main/java/com/swufestu/count/FirActivity.java
@@ -4,9 +4,15 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.nfc.Tag;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,17 +21,50 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.text.DecimalFormat;
 
-public class FirActivity extends AppCompatActivity {
+public class FirActivity extends AppCompatActivity implements Runnable {
    private static final String TAG="FirActivity";
-   double dollar_rate=0.35;
-   double euro_rate=0.28;
-   double won_rate=0.1;
+    double dollar_rate;
+    double euro_rate;
+    double won_rate;
+    Handler handler=null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fir);
+        SharedPreferences sharedPreferences=getSharedPreferences("myrate", Activity.MODE_PRIVATE);
+        //PreferenceManager.getDefaultSharedPreferences(this);
+        String dollar_rate1=sharedPreferences.getString("dollar_rate","");
+        String euro_rate1=sharedPreferences.getString("euro_rate","");
+        String won_rate1=sharedPreferences.getString("won_rate","");
+        dollar_rate=Double.parseDouble(dollar_rate1);
+        euro_rate=Double.parseDouble(euro_rate1);
+        won_rate=Double.parseDouble(won_rate1);
+        TextView result=findViewById(R.id.textView5);
+        handler=new Handler(Looper.myLooper()){
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                Log.i(TAG,"handleMessage:accept Message");
+                if(msg.what==6){
+                    //接收消息
+                   String str= (String)msg.obj;
+                   Log.i(TAG,"handleMessage:str"+str);
+                   result.setText(str);
+                }
+                super.handleMessage(msg);
+            }
+        };
+        Thread thread=new Thread(this);
+        thread.start();//this.run();
     }
 
     public void click3(View view) {
@@ -35,6 +74,10 @@ public class FirActivity extends AppCompatActivity {
         if(s.length()>0&&!s.equals("Input RMB")){
             double v = Double.parseDouble(s);
             if(view.getId()==R.id.button8){
+                //SharedPreferences sharedPreferences=getSharedPreferences("myrate", Activity.MODE_PRIVATE);
+                //PreferenceManager.getDefaultSharedPreferences(this);
+                //String dollar_rate1=sharedPreferences.getString("dollar_rate","");
+                //dollar_rate=Double.parseDouble(dollar_rate1);
                 v=v*dollar_rate;
             }else if(view.getId()==R.id.button9){
                 v=v*euro_rate;
@@ -56,19 +99,27 @@ public class FirActivity extends AppCompatActivity {
     private void extracted() {
         Intent second=new Intent(this,thirdActivity.class);
         //Intent first=new Intent(Intent.ACTION_VIE)
-        second.putExtra("dollar_rate",dollar_rate);
-        second.putExtra("euro_rate",euro_rate);
-        second.putExtra("won_rate",won_rate);
-        Log.i(TAG,""+dollar_rate);
+        //second.putExtra("dollar_rate",dollar_rate);
+        //second.putExtra("euro_rate",euro_rate);
+        //second.putExtra("won_rate",won_rate);
+        //Log.i(TAG,""+dollar_rate);
         startActivityForResult(second,1);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         if(requestCode==1&&resultCode==3){
-            dollar_rate=data.getDoubleExtra("dollar_rate",0.0);
-            euro_rate=data.getDoubleExtra("euro_rate",0.0);
-            won_rate=data.getDoubleExtra("won_rate",0.0);
+            SharedPreferences sharedPreferences=getSharedPreferences("myrate", Activity.MODE_PRIVATE);
+            //PreferenceManager.getDefaultSharedPreferences(this);
+            String dollar_rate1=sharedPreferences.getString("dollar_rate","");
+            String euro_rate1=sharedPreferences.getString("euro_rate","");
+            String won_rate1=sharedPreferences.getString("won_rate","");
+            dollar_rate=Double.parseDouble(dollar_rate1);
+            euro_rate=Double.parseDouble(euro_rate1);
+            won_rate=Double.parseDouble(won_rate1);
+            //dollar_rate=data.getDoubleExtra("dollar_rate",0.0);
+            //euro_rate=data.getDoubleExtra("euro_rate",0.0);
+            //won_rate=data.getDoubleExtra("won_rate",0.0);
         }
         super.onActivityResult(requestCode, resultCode, data);
     }
@@ -85,5 +136,45 @@ public class FirActivity extends AppCompatActivity {
             extracted();
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void run() {
+        Log.i(TAG,"run.........");
+        try {
+            Thread.sleep(3000);
+        }catch (InterruptedException e){
+            e.printStackTrace();
+        }
+        URL url=null;
+        try {
+            Log.i(TAG,"run:访问 url");
+            url=new URL("https://www.usd-cny.com/bankofchina.htm");
+            HttpURLConnection http=(HttpURLConnection) url.openConnection();
+            InputStream in =http.getInputStream();
+            String html=inputStream2String(in);
+            Log.i(TAG,"run:html="+html);
+
+        }catch (MalformedURLException e){
+            e.printStackTrace();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        Message msg=handler.obtainMessage();
+        msg.what=6;
+        msg.obj="Hello from run";
+        handler.sendMessage(msg);
+    }
+    private String inputStream2String(InputStream inputStream) throws IOException{
+        final int bufferSize=1024;
+        final char[]buffer=new char[bufferSize];
+        final StringBuilder out=new StringBuilder();
+        Reader in=new InputStreamReader(inputStream,"gb2312");
+        while (true){
+            int rsz=in.read(buffer,0,buffer.length);
+            if(rsz<0) break;
+            out.append(buffer,0,rsz);
+        }
+        return out.toString();
     }
 }
--- app/src/main/java/com/swufestu/count/thirdActivity.java
+++ app/src/main/java/com/swufestu/count/thirdActivity.java
@@ -2,8 +2,11 @@ package com.swufestu.count;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.View;
 import android.widget.EditText;
@@ -20,13 +23,21 @@ public class thirdActivity extends AppCompatActivity {
         EditText et1=findViewById(R.id.editTextTextPersonName2);
         EditText et2=findViewById(R.id.editTextTextPersonName3);
         EditText et3=findViewById(R.id.editTextTextPersonName4);
-        Intent intent=getIntent();
-        dollar2=intent.getDoubleExtra("dollar_rate",0.0);
-        euro2=intent.getDoubleExtra("euro_rate",0.0);
-        won2=intent.getDoubleExtra("won_rate",0.0);
-        et1.append(String.valueOf(dollar2));
-        et2.append(String.valueOf(euro2));
-        et3.append(String.valueOf(won2));
+        SharedPreferences sharedPreferences=getSharedPreferences("myrate", Activity.MODE_PRIVATE);
+        //PreferenceManager.getDefaultSharedPreferences(this);
+        String dollar_rate1=sharedPreferences.getString("dollar_rate","");
+        String euro_rate1=sharedPreferences.getString("euro_rate","");
+        String won_rate1=sharedPreferences.getString("won_rate","");
+        double dollar_rate=Double.parseDouble(dollar_rate1);
+        double euro_rate=Double.parseDouble(euro_rate1);
+        double won_rate=Double.parseDouble(won_rate1);
+        //Intent intent=getIntent();
+        //dollar2=intent.getDoubleExtra("dollar_rate",0.0);
+        //euro2=intent.getDoubleExtra("euro_rate",0.0);
+        //won2=intent.getDoubleExtra("won_rate",0.0);
+        et1.append(String.valueOf(dollar_rate));
+        et2.append(String.valueOf(euro_rate));
+        et3.append(String.valueOf(won_rate));
     }
 
     public void back(View view) {
@@ -37,9 +48,15 @@ public class thirdActivity extends AppCompatActivity {
         double newdo=Double.parseDouble(et1.getText().toString());
         double neweu=Double.parseDouble(et2.getText().toString());
         double newwo=Double.parseDouble(et3.getText().toString());
-        first.putExtra("dollar_rate",newdo);
-        first.putExtra("euro_rate",neweu);
-        first.putExtra("won_rate",newwo);
+        //first.putExtra("dollar_rate",newdo);
+        //first.putExtra("euro_rate",neweu);
+        //first.putExtra("won_rate",newwo);
+        SharedPreferences sp=getSharedPreferences("myrate",Activity.MODE_PRIVATE);
+        SharedPreferences.Editor editor=sp.edit();
+        editor.putString("dollar_rate",String.valueOf(newdo));
+        editor.putString("euro_rate",String.valueOf(neweu));
+        editor.putString("won_rate",String.valueOf(newwo));
+        editor.apply();
         setResult(3,first);
         finish();
     }
