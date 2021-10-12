4d7e0ad7c6ee4b31602ec74aa4e2c89fe2e3bb1b
zxz0084 <Vilaan@163.com>
2021-10-12 19:42:49 +0800

    13

---
+++ app/src/main/java/com/swufestu/count/MyTask.java
@@ -0,0 +1,63 @@
+package com.swufestu.count;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MyTask  implements  Runnable{
+    private static final String TAG="MyTask";
+    List<String> list=new ArrayList<String>();
+    private Handler handler;
+    public void  setHandler(Handler handler){
+        this.handler=handler;
+    }
+    public void run() {
+        Log.i(TAG,"run.........");
+        int count=0;
+        Bundle bdl=new Bundle();
+        try{
+            Document doc = Jsoup.connect("https://www.usd-cny.com/").get();
+            //Log.i(TAG,doc.title());
+            Elements tables = doc.getElementsByTag("table");//id是唯一的，Tag返回一个集合
+            Element first = tables.first();
+            Elements trs = first.getElementsByTag("tr");
+            trs.remove(0);
+            for (Element tr :trs){
+                //Log.i(TAG,"run:tr="+tr);
+                Elements tds = tr.getElementsByTag("td");
+                Element td1 = tds.get(0);
+                Element td2 = tds.get(4);
+                Log.i(TAG,"td1="+td1.text()+" \t td2="+td2.text());
+                list.add(td1.text()+"==>"+td2.text());
+                /*
+                if("美元".equals(td1.text())){
+                    String rate1=td2.text();
+                    bdl.putDouble("dollar_rate",100/Double.parseDouble(rate1));
+                }else if("欧元".equals(td1.text())){
+                    String rate1=td2.text();
+                    bdl.putDouble("euro_rate",100/Double.parseDouble(rate1));
+                }else if("韩元".equals(td1.text())){
+                    String rate1=td2.text();
+                    bdl.putDouble("won_rate",100/Double.parseDouble(rate1));
+                }*/
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        bdl.putInt("num",count);
+        Message msg=handler.obtainMessage();
+        msg.what=6;
+        msg.obj=list;
+        handler.sendMessage(msg);
+    }
+}
---
+++ app/src/main/java/com/swufestu/count/secondActivity.java
@@ -0,0 +1,50 @@
+package com.swufestu.count;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class secondActivity extends ListActivity {
+    private static final String TAG="second";
+    Handler handler=null;
+    int count;
+    Bundle bundle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        List<String> list1=new ArrayList<String>();
+        for(int i=1;i<100;i++){
+            list1.add("item"+i);
+        }
+        handler = new Handler(Looper.myLooper()){
+            @Override
+            public void handleMessage(@NonNull Message msg) {
+                super.handleMessage(msg);
+                if(msg.what==6){
+                    ArrayList<String> rlist=(ArrayList<String>)msg.obj;
+                    ListAdapter adapter=new ArrayAdapter<String>(secondActivity.this, android.R.layout.simple_list_item_1,rlist);
+                    setListAdapter(adapter);
+                }
+            }
+        };
+        MyTask task=new MyTask();
+        task.setHandler(handler);
+        Thread thread=new Thread(task);
+        thread.start();
+
+
+        //setContentView(R.layout.activity_second);
+    }
+}
